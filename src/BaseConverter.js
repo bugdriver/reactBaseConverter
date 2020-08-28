4c68293f72b8f2d46a8c0fe3e447eb68c0170a1e
rajat <rajat4113@gmail.com>
2020-08-28 12:50:13 +0530

    convert numbers into different base

---
+++ src/BaseConverter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { InputBox, range } from './inputBox';
+
+class BaseConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    if (!value) value = 0;
+    this.setState({ value });
+  }
+
+  render() {
+    const inputBoxes = range(2, 17).map((base) => {
+      return (
+        <div key={base} style={{ margin: '0.5em' }}>
+          <span>Base {base} : </span>
+          <InputBox
+            base={base}
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </div>
+      );
+    });
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {inputBoxes}
+      </div>
+    );
+  }
+}
+
+export default BaseConverter;
